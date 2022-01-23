7dfe7b9b5558141d19f8a850625311b8b24e8d4e
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-01-23 15:42:59 -0500

    set up server.js file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Turn on routes
+app.use(routes);
+
+// Turn on connection to database and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Server is now running'));
+});
