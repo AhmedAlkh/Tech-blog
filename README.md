1799aff4716c1f610dc2dc70e3210ebcd124c63d
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-01-23 14:55:32 -0500

    user table columns and config defined

--- README.md
+++ README.md
@@ -37,3 +37,7 @@ SO THAT I can publish articles, blog posts, and my thoughts and opinions
 * []THEN I am signed out of the site
 * []WHEN I am idle on the site for more than a set time
 * []THEN I am able to view comments but I am prompted to log in again before I can add, update, or delete comments
+
+User - log in and sign up
+Post - User can create posts
+Comment - User can comment on posts
---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create User model
+class User extends Model {}
+
+// Define table columns and configuration
+
+// Table column definitions
+User.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: Datatypes.STRING,          
+            allowNull: false
+        },
+        password: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5]
+            }
+        }
+    },
+    // Table configuration options
+    {
+        // pass in our imported sequelize connection (the direct connection to our database)
+        sequelize,
+        // Don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // Don't pluralize name of database table
+        freezeTableName: true,
+        // Use underscores instead of camel-casing
+        underscored: true,
+        // make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
